301d174d04a56906733b31f5a0179e02e9c7224a
VitaliiVoloboiev <83081678+VitaliiVoloboiev@users.noreply.github.com>
2022-02-04 17:29:12 +0000

    Update templ-aws.tf

--- templ-aws.tf
+++ templ-aws.tf
@@ -6,7 +6,7 @@ provider "aws" {
    # Amazon Linux AMI 2017.03.1 (HVM)
    ami           = "ami-657bd20a"
    instance_type = "t2.micro"
-
+# autoapply GO!
    tags = {
      Name = "${var.vm_name}"
    }
